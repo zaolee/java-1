d8bdca139a61820b957e4745ae5ac78c7930cf67
DESKTOP-GL37BQK\user2 <lee970808@naver.com>
2022-11-03 21:27:56 +0900

    자바프로그래밍 시작하기

---
+++ A_Chapter01/src/hello/Hello.java
@@ -0,0 +1,16 @@
+package hello;
+
+public class Hello { // 클래스 이름 대소문자 주의!
+
+	public static void main(String[] args) {
+		// 실행될 문장(코드)들을 적는다.
+		System.out.println("Hello");
+		System.out.println("하의!");
+		System.out.println("이소연");
+	}
+}
+/*
+ 주석 
+ - 소스 코드와 무관하고 코드로 인식되지 않는다.
+ - 주로 코드에 대한 설명을 작성할 때 사용한다.
+ */
